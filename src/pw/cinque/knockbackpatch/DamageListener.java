8ad02185f543bef3b49d694e466eb8e21925968c
Fyu <cinquepvp@gmail.com>
2016-04-06 16:14:04 +0200

    Fix relog by cancelling the vanilla knockback and applying our own

---
+++ src/pw/cinque/knockbackpatch/DamageListener.java
@@ -0,0 +1,87 @@
+package pw.cinque.knockbackpatch;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerVelocityEvent;
+import org.bukkit.util.Vector;
+
+public class DamageListener implements Listener {
+
+	private Field fieldPlayerConnection;
+	private Method sendPacket;
+	private Constructor<?> packetVelocity;
+
+	public DamageListener() {
+		try {			
+			Class<?> entityPlayerClass = Class.forName("net.minecraft.server." + KnockbackPatch.getInstance().getCraftBukkitVersion() + ".EntityPlayer");
+			Class<?> packetVelocityClass = Class.forName("net.minecraft.server." + KnockbackPatch.getInstance().getCraftBukkitVersion() + ".PacketPlayOutEntityVelocity");
+			Class<?> playerConnectionClass = Class.forName("net.minecraft.server." + KnockbackPatch.getInstance().getCraftBukkitVersion() + ".PlayerConnection");
+
+			// Get the fields here to improve performance later on			
+			this.fieldPlayerConnection = entityPlayerClass.getField("playerConnection");
+			this.sendPacket = playerConnectionClass.getMethod("sendPacket", packetVelocityClass.getSuperclass());
+			this.packetVelocity = packetVelocityClass.getConstructor(int.class, double.class, double.class, double.class);
+		} catch (ClassNotFoundException | NoSuchFieldException | SecurityException | NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@EventHandler
+	public void onPlayerVelocity(PlayerVelocityEvent event) {
+		Player player = event.getPlayer();
+		EntityDamageEvent lastDamage = player.getLastDamageCause();
+
+		if (lastDamage == null || !(lastDamage instanceof EntityDamageByEntityEvent)) {
+			return;
+		}
+
+		// Cancel the vanilla knockback
+		if (((EntityDamageByEntityEvent) lastDamage).getDamager() instanceof Player) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+		if (!(event.getEntity() instanceof Player) || !(event.getDamager() instanceof Player)) {
+			return;
+		}
+
+		if (event.isCancelled()) {
+			return;
+		}
+
+		Player damaged = (Player) event.getEntity();
+		Player damager = (Player) event.getDamager();
+
+		double horMultiplier = KnockbackPatch.getInstance().getHorMultiplier();
+		double verMultiplier = KnockbackPatch.getInstance().getVerMultiplier();
+		double sprintMultiplier = damager.isSprinting() ? 0.8D : 0.4D;
+		double kbMultiplier = damager.getItemInHand() == null ? 0 : damager.getItemInHand().getEnchantmentLevel(Enchantment.KNOCKBACK) * 0.2D;
+
+		Vector knockback = damager.getLocation().getDirection().normalize();
+		knockback.setX((knockback.getX() * sprintMultiplier + kbMultiplier) * horMultiplier);
+		knockback.setY(0.35D * verMultiplier);
+		knockback.setZ((knockback.getZ() * sprintMultiplier + kbMultiplier) * horMultiplier);
+		
+		try {
+			// Send the velocity packet immediately instead of using setVelocity, which fixes the 'relog bug'
+			Object entityPlayer = damaged.getClass().getMethod("getHandle").invoke(damaged);
+			Object playerConnection = fieldPlayerConnection.get(entityPlayer);
+			Object packet = packetVelocity.newInstance(damaged.getEntityId(), knockback.getX(), knockback.getY(), knockback.getZ());
+			sendPacket.invoke(playerConnection, packet);
+		} catch (SecurityException | IllegalArgumentException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/pw/cinque/knockbackpatch/KnockbackPatch.java
+++ src/pw/cinque/knockbackpatch/KnockbackPatch.java
@@ -1,5 +1,90 @@
 package pw.cinque.knockbackpatch;
 
-public class KnockbackPatch {
+import org.apache.commons.lang.math.NumberUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class KnockbackPatch extends JavaPlugin {
+
+	private static KnockbackPatch instance;
+	
+	private String craftBukkitVersion;
+	private double horMultiplier = 1D;
+	private double verMultiplier = 1D;
+	
+	@Override
+	public void onEnable() {
+		instance = this;
+		
+		getConfig().options().copyDefaults(true);
+		getConfig().addDefault("knockback-multiplier.horizontal", 1D);
+		getConfig().addDefault("knockback-multiplier.vertical", 1D);
+		saveConfig();
+		
+		// CraftBukkit version is used to access NMS classes using reflection
+		this.craftBukkitVersion = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+		this.horMultiplier = getConfig().getDouble("knockback-multiplier.horizontal");
+		this.verMultiplier = getConfig().getDouble("knockback-multiplier.vertical");
+		
+		Bukkit.getPluginManager().registerEvents(new DamageListener(), this);
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (!sender.hasPermission("knockbackpatch.setknockback")) {
+			sender.sendMessage(ChatColor.RED + "No permission.");
+			return true;
+		}
+		
+		if (args.length < 2){
+			sender.sendMessage(ChatColor.RED + "Usage: /" + label + " <horizontal multiplier> <vertical multiplier>.");
+			return true;
+		}
+		
+		double horMultiplier = NumberUtils.toDouble(args[0], -1D);
+		double verMultiplier = NumberUtils.toDouble(args[1], -1D);
+		
+		if (horMultiplier < 0D || verMultiplier < 0D) {
+			sender.sendMessage(ChatColor.RED + "Invalid horizontal/vertical multiplier!");
+			return true;
+		}
+		
+		this.horMultiplier = horMultiplier;
+		this.verMultiplier = verMultiplier;
+		
+		getConfig().set("knockback-multiplier.horizontal", horMultiplier);
+		getConfig().set("knockback-multiplier.vertical", verMultiplier);
+		saveConfig();
+		
+		sender.sendMessage(ChatColor.GREEN + "Successfully updated the knockback multipliers!");
+		return true;
+	}
+	
+	public static KnockbackPatch getInstance() {
+		return instance;
+	}
+
+	public String getCraftBukkitVersion() {
+		return craftBukkitVersion;
+	}
+	
+	public double getHorMultiplier() {
+		return horMultiplier;
+	}
+
+	public void setHorMultiplier(double horMultiplier) {
+		this.horMultiplier = horMultiplier;
+	}
+
+	public double getVerMultiplier() {
+		return verMultiplier;
+	}
+
+	public void setVerMultiplier(double verMultiplier) {
+		this.verMultiplier = verMultiplier;
+	}
+	
 }
